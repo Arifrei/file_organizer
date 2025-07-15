70bd306c6d80576ffa48b9dfd2085968dceca91b
Ari <aronf11121@gmail.com>
2025-07-15 17:41:31 -0400

    Added flask routes

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import shutil
-from os import mkdir
+from flask import Flask, render_template, redirect, url_for, request
+from pyexpat.errors import messages
+
+app = Flask(__name__)
 
 FILE_TYPES = {
     "Documents": ['.pdf', '.docx', '.txt', '.xls', '.xlsx', '.pptx'],
@@ -18,17 +21,26 @@ def get_category(extension):
     for category, extensions in FILE_TYPES.items():
         if extension.lower() in extensions:
             return category
-    return "others"
+    return "Others"
 
-def organize_files(filepath):
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/organize", methods=["GET", "POST"])
+def organize_files():
+    message = ""
+    filepath = request.form.get("folder")
     if not os.path.isdir(filepath):
-        print("Invalid filepath")
-        return
+        message = "Invalid filepath"
+        return render_template("index.html", status=message)
 
+    file_found = False
     for item in os.listdir(filepath):
         item_path = os.path.join(filepath, item)
 
         if os.path.isfile(item_path):
+            file_found = True
             _, ext = os.path.splitext(item)
             category = get_category(ext)
             category_folder = os.path.join(filepath, category)
@@ -39,13 +51,25 @@ def organize_files(filepath):
             dest_path = os.path.join(category_folder, item)
             shutil.move(item_path, dest_path)
             moved_files.append((dest_path, item_path))
+            message = "Files organized"
+    if not file_found:
+        message = "No files to organize"
+    return render_template("index.html", status=message)
 
-def undo_last_move():
-    while moved_files:
-        moved_to, original = moved_files.pop()
-        if os.path.exists(moved_to):
-            shutil.move(original, moved_to)
-        else:
-            print(f"File not found for undo: {moved_to}")
+@app.route("/undo")
+def undo():
+    message = ""
+    if moved_files:
+        while moved_files:
+            moved_to, original = moved_files.pop()
+            if os.path.exists(moved_to):
+                shutil.move(moved_to, original)
+                message= "Undo successful"
+            else:
+                message = f"File not found for undo: {moved_to}"
+    else:
+        message = "No files to move back"
+    return render_template("index.html", status=message)
 
-organize_files("C:/Users/Hello/Documents")
+if __name__ == "__main__":
+    app.run(debug=True)
